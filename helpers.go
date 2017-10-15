a3beb2e1494867b9f3f37402fe1c6b1f1283c4fd
Requilence <requilence@gmail.com>
2017-10-15 16:25:37 +0300

    Helper added: NewEditMessageTextAndInlineKeyboard

--- helpers.go
+++ helpers.go
@@ -486,6 +486,18 @@ func NewEditMessageText(chatID int64, messageID int, text string) EditMessageTex
 	}
 }
 
+// NewEditMessageTextAndInlineKeyboard allows you to edit the text and inline keyboard a message.
+func NewEditMessageTextAndInlineKeyboard(chatID int64, messageID int, text string, replyMarkup *InlineKeyboardMarkup) EditMessageTextConfig {
+	return EditMessageTextConfig{
+		BaseEdit: BaseEdit{
+			ChatID:      chatID,
+			MessageID:   messageID,
+			ReplyMarkup: replyMarkup,
+		},
+		Text: text,
+	}
+}
+
 // NewEditMessageCaption allows you to edit the caption of a message.
 func NewEditMessageCaption(chatID int64, messageID int, caption string) EditMessageCaptionConfig {
 	return EditMessageCaptionConfig{
